4e6e5bb7ee5ed11f91a35cf631b4f9b0ad91a1bb
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-10-11 23:09:26 +0200

    nmhost

--- internal/host/host.go
+++ internal/host/host.go
@@ -1,18 +1,19 @@
 package host
 
 import (
+	"bufio"
 	"encoding/binary"
 	"encoding/json"
 	"errors"
+	"io"
 	"math"
 	"os"
 	"sync"
 )
 
 type Host struct {
-	lastId      uint32
-	lastIdMutex sync.Mutex
-	stdoutMutex sync.Mutex
+	mu        sync.Mutex
+	lastMsgId uint32
 }
 
 type Address uint8
@@ -24,63 +25,106 @@ const (
 	NativeHost
 )
 
+type Command uint8
+
 type MessageHeader struct {
 	Id        uint32  `json:"id"`
 	Source    Address `json:"source"`
 	Target    Address `json:"target"`
+	Cmd       Command `json:"cmd"`
 	Webpage   uint16  `json:"webpage,omitempty"`
 	RequestId uint32  `json:"requestId,omitempty"`
 }
 
+type Message struct {
+	Header  MessageHeader   `json:"header"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+const MaxMsgSize = 1048576 // 1 MiB
+const ReaderBufSize = 4096 // 4 KiB
+
 func NewHost() (*Host, error) {
 	return &Host{
-		lastId:      0,
-		lastIdMutex: sync.Mutex{},
-		stdoutMutex: sync.Mutex{},
+		mu:        sync.Mutex{},
+		lastMsgId: 0,
 	}, nil
 }
 
-func (host *Host) Run() {
-	// TODO
+func (host *Host) Run() error {
+	var msgSize uint32
+	reader := bufio.NewReaderSize(os.Stdin, ReaderBufSize)
+	readMsgSizeErr := binary.Read(reader, binary.NativeEndian, &msgSize)
+	for readMsgSizeErr == nil {
+		msgBytes := make([]byte, msgSize)
+		_, err := io.ReadFull(reader, msgBytes)
+		if err != nil {
+			return errors.New("failed to read message: " + err.Error())
+		}
+		var msg Message
+		err = json.Unmarshal(msgBytes, &msg)
+		if err != nil {
+			return errors.New("failed to unmarshal message: " + err.Error())
+		}
+		host.handleMessage(&msg)
+		readMsgSizeErr = binary.Read(reader, binary.NativeEndian, &msgSize)
+	}
+	if readMsgSizeErr != io.EOF {
+		return errors.New("failed to read message size: " + readMsgSizeErr.Error())
+	}
+	return nil
 }
 
-func (host *Host) Send(target Address, webpage uint16, requestId uint32, payload *interface{}) (uint32, error) {
-	header := MessageHeader{
-		Id:        host.generateId(),
-		Source:    NativeHost,
-		Target:    target,
-		Webpage:   webpage,
-		RequestId: requestId,
+func (host *Host) Send(target Address, cmd Command, webpage uint16, requestId uint32, payload *json.RawMessage) (uint32, error) {
+	msg := Message{
+		Header: MessageHeader{
+			Source:    NativeHost,
+			Target:    target,
+			Cmd:       cmd,
+			Webpage:   webpage,
+			RequestId: requestId,
+		},
+		Payload: *payload,
 	}
-	headerJson, err := json.Marshal(&header)
+	msgBytes, err := json.Marshal(&msg)
 	if err != nil {
-		return header.Id, errors.New("failed to marshal header: " + err.Error())
+		return 0, errors.New("failed to marshal message: " + err.Error())
 	}
-	payloadJson, err := json.Marshal(payload)
-	if err != nil {
-		return header.Id, errors.New("failed to marshal payload: " + err.Error())
+	msgSize := len(msgBytes)
+	if msgSize > MaxMsgSize {
+		return 0, errors.New("message too large")
 	}
-	length := len(headerJson) + len(payloadJson)
-	if length > 1048576 {
-		return header.Id, errors.New("message too long")
-	}
-	host.stdoutMutex.Lock()
-	binary.Write(os.Stdout, binary.NativeEndian, uint32(length))
-	os.Stdout.Write(headerJson)
-	os.Stdout.Write(payloadJson)
-	os.Stdout.Sync()
-	host.stdoutMutex.Unlock()
-	return header.Id, nil
-}
-
-func (host *Host) generateId() uint32 {
-	host.lastIdMutex.Lock()
-	if host.lastId < math.MaxUint32 {
-		host.lastId++
+	host.mu.Lock()
+	if host.lastMsgId < math.MaxUint32 {
+		msg.Header.Id = host.lastMsgId + 1
 	} else {
-		host.lastId = 1
+		msg.Header.Id = 1
 	}
-	id := host.lastId
-	host.lastIdMutex.Unlock()
-	return id
+	err = os.Stdout.Sync()
+	if err != nil {
+		host.mu.Unlock()
+		return 0, errors.New("failed to sync stdout: " + err.Error())
+	}
+	err = binary.Write(os.Stdout, binary.NativeEndian, uint32(msgSize))
+	if err != nil {
+		host.mu.Unlock()
+		return 0, errors.New("failed to write message size: " + err.Error())
+	}
+	_, err = os.Stdout.Write(msgBytes)
+	if err != nil {
+		host.mu.Unlock()
+		return 0, errors.New("failed to write message: " + err.Error())
+	}
+	err = os.Stdout.Sync()
+	if err != nil {
+		host.mu.Unlock()
+		return 0, errors.New("failed to sync stdout: " + err.Error())
+	}
+	host.lastMsgId = msg.Header.Id
+	host.mu.Unlock()
+	return msg.Header.Id, nil
+}
+
+func (host *Host) handleMessage(msg *Message) {
+	// TODO
 }
